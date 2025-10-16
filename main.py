0c86be890cd4019cc63bd96bfae0a363931bd62b
Xavier P <126206539+TheWebsiteMan@users.noreply.github.com>
2025-10-16 23:36:32 +0000

    done with main

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ app = Flask(__name__)
 @app.route("/index.html", methods=["GET"])
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("/index.html")
+    data = dbHandler.listExtension()
+    return render_template("/index.html", content=data)
+
+
+@app.route("/add.html", methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        email = request.form["email"]
+        name = request.form["name"]
+        dbHandler.insertContact(email, name)
+        return render_template("/add.html", is_done=True)
+    else:
+        return render_template("/add.html")
 
 
 if __name__ == "__main__":
